5b47a69e8ccbf60a281657416f9b415714e2b6af
ipheghe <iphegheovie@gmail.com>
2021-03-06 19:47:55 +0100

    remove unused file

--- src/helpers/formatter.ts
+++ src/helpers/formatter.ts
@@ -1,8 +1,9 @@
-const currencyFormatter = (amount: number, currency: string) => {
+// helper function to convert currency
+const formatter = (amount: number, currency: string) => {
     return amount?.toLocaleString(`en-${currency?.slice?.(0, 2)}`, {
       style: "currency",
       currency,
     });
   };
 
-export default currencyFormatter;
+export default formatter;
